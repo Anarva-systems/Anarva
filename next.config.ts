abe6f12907ff03a8af22578ece2e622cba2ad27a
Mohan Ashapu <ashapumohan123@gmail.com>
2026-03-08 14:46:19 +0530

    Fix Turbopack build error

--- next.config.ts
+++ next.config.ts
@@ -2,9 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "standalone", // <--- ADD THIS LINE
-  turbopack: {
-    root: __dirname,
-  },
   images: {
     remotePatterns: [
       {
